ef2b9cbf3406af2b8fb6c5634f8ac07d78557c1d
helmer <helmer.morales1@gmail.com>
2021-12-26 23:30:42 -0300

    reconfiguracion

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import (
+    Blueprint, 
+    render_template, 
+    request, 
+    redirect, 
+    url_for,
+    current_app,
+    Flask
+)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('portafolio/index.html')
+    
+
+@app.route('/mail')
+def mail():
+        return render_template('portafolio/sent_mail.html')
+        
+if __name__ == "__main__":
+	app.run()
+    
